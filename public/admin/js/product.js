5dac776cb83539b1e927488e15fa46649d8de447
siyu950824 <1450806724@qq.com>
2019-12-09 21:59:15 +0800

    pc

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -1,6 +1,7 @@
 $(function(){
   var page=1;
-  var pageSize=2
+  var pageSize=2;
+  var arr=[]
   function render(){
     $.ajax({
       type:'get',
@@ -25,4 +26,137 @@ $(function(){
     })
   }
   render()
+  $('.btn-add').on('click', function () {
+    $('.addModal').modal('show')
+    $.ajax({
+      type: 'get',
+      url: '/category/querySecondCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      success: function (info) {
+        $('.dropdown-menu').html(template('dropdownTpl', info))
+      }
+    })
+  })
+  $('.dropdown-menu').on('click', 'a', function () {
+    var content = $(this).text()
+    var id = $(this).data('id')
+    $('.text').text(content)
+    $('[name=brandId]').val(id)
+    $('form').data('bootstrapValidator').updateStatus('brandId', 'VALID')
+  })
+
+  $('#file').fileupload({
+    done: function (e, data) {
+      var info = data.result
+      console.log(data);
+      
+      console.log(info)
+      // $('.brandLogo').attr('src', info.picAddr)
+      $('<img src="'+info.picAddr+'" class="brandLogo" width="100"  height="100" alt="">').appendTo('.img_box')
+    
+    arr.push(data.result)
+     if(arr.length>=3) {
+      $('[name=picArr]').val(JSON.stringify(arr))
+      $('form').data('bootstrapValidator').updateStatus('picArr', 'VALID')
+     }
+    }
+  })
+  $('form').bootstrapValidator({
+    excluded: [],
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      brandId: {
+        validators: {
+          notEmpty: {
+            message: '请选择二级分类'
+          }
+        }
+      },
+      proName: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品名称'
+          }
+        }
+      },
+      proDesc: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品描述'
+          }
+        }
+      },
+     num: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品的库存'
+          },
+          regexp:{
+            regexp:/^[1-9]\d{0,4}$/,
+            message: '库存范围1~99999'
+          }
+        }
+      },
+      size: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品尺码'
+          },
+          regexp:{
+            regexp:/^\d{2}-\d{2}$/,
+            message: '请输入正确的尺码格式：比如32-42'
+          }
+        }
+      },
+      oldPrice: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品原价'
+          }
+        }
+      },
+      Price: {
+        validators: {
+          notEmpty: {
+            message: '请输入商品价格'
+          }
+        }
+      },
+      picArr: {
+        validators: {
+          notEmpty: {
+            message: '请上传3张图片'
+          }
+        }
+      },
+    }
+  })
+  $('form').on('success.form.bv', function(e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: '/product/addProduct',
+      data: $('form').serialize(),
+      success: function(info) {
+        console.log(info)
+        if (info.success) {
+          $('.addModal').modal('hide')
+          $('form').data('bootstrapValidator').resetForm(true)
+          page = 1
+          render()
+          $('.text').text('请选择二级分类')
+          $('.img_box img').remove()
+          arr = []
+        }
+      }
+    })
+  })
+ 
 })
--- public/admin/js/second.js
+++ public/admin/js/second.js
@@ -1,16 +1,16 @@
-$(function(){
-  var page=1;
-  var pageSize=5
-  function render(){
+$(function () {
+  var page = 1;
+  var pageSize = 5
+  function render() {
     $.ajax({
-      type:'get',
-      url:'/category/querySecondCategoryPaging',
-      data:{
-      page:page,
-      pageSize:pageSize
+      type: 'get',
+      url: '/category/querySecondCategoryPaging',
+      data: {
+        page: page,
+        pageSize: pageSize
       },
-      success:function(info){
-        $('tbody').html(template('tpl',info))
+      success: function (info) {
+        $('tbody').html(template('tpl', info))
         $('#page').bootstrapPaginator({
           bootstrapMajorVersion: 3,
           currentPage: page,
@@ -24,21 +24,87 @@ $(function(){
     })
   }
   render()
-$('.btn-add').on('click',function(){
-  $('.addModal').modal('show')
-  $.ajax({
-    type:'get',
-    url:'/category/queryTopCategoryPaging',
-    data:{
-      page:1,
-      pageSize:100
-    },
-    success:function(info){
-      console.log(info);
-     $('.dropdown-menu').html(template('dropdownTpl',info))
+  $('.btn-add').on('click', function () {
+    $('.addModal').modal('show')
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      success: function (info) {
+        $('.dropdown-menu').html(template('dropdownTpl', info))
+      }
+    })
+  })
+  // 一级菜单
+  $('.dropdown-menu').on('click', 'a', function () {
+    var content = $(this).text()
+    var id = $(this).data('id')
+    $('.text').text(content)
+    $('[name=categoryId]').val(id)
+    $('form').data('bootstrapValidator').updateStatus('categoryId', 'VALID')
+  })
+  // 提交图片
+  $('#file').fileupload({
+    done: function (e, data) {
+      var info = data.result
+      $('.brandLogo').attr('src', info.picAddr)
+      $('[name=brandLogo]').val(info.picAddr)
+      $('form').data('bootstrapValidator').updateStatus('brandLogo', 'VALID')
     }
   })
-})
+  $('form').bootstrapValidator({
+    excluded: [],
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: '请选择一级分类'
+          }
+        }
+      },
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: '请输入品牌的名称'
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: '请上传品牌图片'
+          }
+        }
+      }
+    }
+  })
+
+  $('form').on('success.form.bv', function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('form').serialize(),
+      success: function (info) {
+        if (info.success){
+          $('.addModal').modal('hide')
+          $('form').data('bootstrapValidator').resetForm(true)
+        }
+        page = 1
+        render()
+        $('.brandLogo').attr('src', 'images/none.png')
+        $('.text').text('请选择一级分类')
+      }
+    })
+  })
 })
 
 
