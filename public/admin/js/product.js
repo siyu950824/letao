8d2f6a21ac70dd0e4b46f19dc099686ddf1ae7be
siyu950824 <1450806724@qq.com>
2019-12-09 14:35:17 +0800

    第三天

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -10,7 +10,17 @@ $(function(){
         pageSize:pageSize
       },
       success:function(info){
-        console.log(info)
+        console.log(info);
+       $('tbody').html(template('tpl',info))
+       $('#page').bootstrapPaginator({
+        bootstrapMajorVersion: 3,
+        currentPage: page,
+        totalPages: Math.ceil(info.total / info.size),
+        onPageClicked: function (a, b, c, d) {
+          page = d
+          render()
+        }
+      });
       }
     })
   }
